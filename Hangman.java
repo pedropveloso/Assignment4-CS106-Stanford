8089412a95d72d803cc3017b51416ca0eb95b5e0
S <s>
2013-01-02 11:58:09 +0000

    {"type":"TI", "runs":"2"}

--- Hangman.java
+++ Hangman.java
@@ -12,7 +12,10 @@ import acm.util.*;
 import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
-/*	public void init() {
+
+private static final int GUESSES = 8;	
+	
+	/*	public void init() {
 		canvas = new HangmanCanvas();
 		add(canvas);
 	}*/
@@ -20,20 +23,41 @@ public class Hangman extends ConsoleProgram {
 	public void run() {
 		println ("Welcome to Hangman!");
 		chooseWord();
-/*		while (!gameOver) {
+		guess = GUESSES;
+		while (!gameOver) {
 			showWord();
-			gess();
-		}*/
+			newGuess();
+		}
+	}
+	
+	private boolean gameOver() {
+		return (win) || (loose);
 	}
 	
 	private void chooseWord() {
 		int index = rgen.nextInt(0,HangmanLexicon.getWordCount()-1);
 		word = HangmanLexicon.getWord(index);
-		println (word);
+		len = word.length();
+		result = "";
+		for (int i=0; i<len; i++) {
+			result = result + "-";
+		}
 	}
 	
+	private void showWord() {
+		println ("The word now looks like this: " + result);
+		switch (guess) {
+		case 1: println("You have only one guess left");
+		default: println("You have " + guess + "guesses left.");
+		}
+	}
 	
 //private HangmanCanvas canvas;
 private String word; //the secret word of the game	
+private int len; // the length of the secret word
+private int guess; //the number of guesses the user has
+private String result; //the current state of the guesses word
+private boolean win; //true when user guesses all the letters of the word
+private boolean loose; //true when the player makes 8 wrong guesses
 private RandomGenerator rgen = RandomGenerator.getInstance();    
 }
