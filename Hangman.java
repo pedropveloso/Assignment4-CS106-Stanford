5514ab293afdb17e065b1d20de8255e32639758b
S <s>
2013-01-02 23:01:59 +0000

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -36,7 +36,7 @@ private static final int GUESSES = 8;
 	
 	private void setup() {
 		guess = GUESSES;
-		int index = rgen.nextInt(0, 9);
+		int index = rgen.nextInt(0, HangmanLexicon.getWordCount()-1);
 		word = HangmanLexicon.getWord(index);
 		len = word.length();
 		result = "";
