b1bc92b37727afa81aaf52df98902cce55d37081
S <s>
2013-01-15 10:53:15 +0000

    {"type":"TI", "runs":"0"}

--- HangmanLexicon.java
+++ HangmanLexicon.java
@@ -6,28 +6,37 @@
  */
 
 import acm.util.*;
+import java.io.*;
+import java.util.*;
 
 public class HangmanLexicon {
 
-/** Returns the number of words in the lexicon. */
+/* ArrayList of with the words */
+private ArrayList<String> lineList;
+	
+	public HangmanLexicon() {
+		try {
+			BufferedReader rd = new BufferedReader (new FileReader ("ShorterLexicon.txt"));
+			lineList = new ArrayList<String>();
+			while (true) {
+				String line = rd.readLine();
+				if (line==null) break;
+				lineList.add(line);
+			}
+			rd.close();
+		} catch (IOException ex) {
+			throw new ErrorException(ex);
+		}
+	}
+	
+	/** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		int size = lineList.size();
+		return size;
 	}
 
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
+		return lineList.get(index);
 	};
 }
